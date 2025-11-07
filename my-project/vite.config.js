45aef81b4bac35e8aadbfb149882f266e81e8fc7
Swarnav Das <swarnavofcl15@gmail.com>
2025-11-07 23:20:55 +0530

    khul ja sim sim

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: './',
+  base: './',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://div-github-io.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
